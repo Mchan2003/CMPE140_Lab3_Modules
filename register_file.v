557fab6451e5d664c6a44678891e7011fcadf111
Michael Chan <mchan100work@gmail.com>
2025-02-24 11:40:38 -0800

    finished test register file

--- register_file.v
+++ register_file.v
@@ -1,25 +1,31 @@
-module instruction_memory(
-    input rst_n,
-    input clk,
-    input rsi1, 
-    input rsi2,
-    input data,
-    input rdi,
-    input we,
-    output rsi1,
-    output rsi2
+module register_file(
+    input wire rst_n,
+    input wire clk,
+    input wire [0:4] rs1, 
+    input wire [0:4] rs2,
+    input wire [0:4] rd,
+    input wire [0:31] wd,
+    input wire we,
+    output wire [0:31] data_rs1,
+    output wire [0:31] data_rs2
 );
 reg [31:0] register [31:0];
 
-assign rs1 = register[rsi1];
-assign rs2 = register[rsi2];
+assign data_rs1 = register[rs1];
+assign data_rs2 = register[rs2];
+
+integer i;
 
 always @ (posedge clk)
 begin
+    if (rst_n)begin
+        for (i = 0; i < 32; i = i + 1) begin
+            register[i] <= 32'b0;
+        end    
+    end
     if(we) begin
-        register[rdi] <= data;
+        register[rd] <= wd;
     end
 end
-
 
 endmodule
